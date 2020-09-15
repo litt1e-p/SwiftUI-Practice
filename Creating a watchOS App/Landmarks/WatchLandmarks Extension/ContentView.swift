cd7309ff95dc50955c070ea6d4aa4cff7ec6f528
litt1e-p <free9fw@gmail.com>
2020-09-15 16:40:18 +0800

    Creating a watchOS App

---
+++ Creating a watchOS App/Landmarks/WatchLandmarks Extension/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Extension
+//
+//  Created by mac on 2020/9/14.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList { WatchLandmarkDetail(landmark: $0) }
+        .environmentObject(UserData())
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList { WatchLandmarkDetail(landmark: $0) }
+        .environmentObject(UserData())
+    }
+}
